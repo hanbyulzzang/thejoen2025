f91767e04ac0f953a50c0549b7bead1321f06338
TJ <TJ@DESKTOP-8FSFFJK>
2025-03-15 17:13:18 +0900

    ff

---
+++ Jungol/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		scanner sc = new scanner(System.in);
+		int A, B;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A + B);
+	}
+
+}
